6ed473fec89897327b5638a2f82f11c143040c35
RimshaArfeen <rimshaarfeen61@gmail.com>
2025-12-01 19:56:22 +0500

    Fix Prisma

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,15 +1,19 @@
+
 //app/api/orders/route.js
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma, Prisma } from "@/lib/prisma";
 
 export async function GET(request) {
      try {
           const { searchParams } = new URL(request.url);
-          const userId = searchParams.get("userId");
+          let userId = searchParams.get("userId");
 
           if (!userId) {
                return NextResponse.json({ error: "Missing userId" }, { status: 400 });
           }
+
+          // Cast to MongoDB ObjectId
+          userId = new Prisma.ObjectId(userId);
 
           const orders = await prisma.order.findMany({
                where: { userId },
@@ -26,5 +30,3 @@ export async function GET(request) {
           return NextResponse.json({ error: "Server error" }, { status: 500 });
      }
 }
-
-
